8638ffbb67c6b4c013d12361e86e033fdf75b089
Abhishek Mangle <abhishekmangle816@gmail.com>
2026-04-26 17:02:23 +0530

    fixed api connection

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import useAuthStore from '../store/authStore';
 
 const axiosClient = axios.create({
-  baseURL: '/api/',
+  baseURL: 'https://ayursutra-backend-vv6w.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -11,9 +11,11 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use(
   (config) => {
     const token = useAuthStore.getState().accessToken;
+
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
@@ -23,26 +25,38 @@ axiosClient.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
+
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
+
       try {
         const refreshToken = useAuthStore.getState().refreshToken;
+
         if (!refreshToken) {
           useAuthStore.getState().logout();
           return Promise.reject(error);
         }
-        const response = await axios.post('/api/accounts/token/refresh/', {
-          refresh: refreshToken,
-        });
+
+        const response = await axios.post(
+          'https://ayursutra-backend-vv6w.onrender.com/api/accounts/token/refresh/',
+          {
+            refresh: refreshToken,
+          }
+        );
+
         const newAccess = response.data.access;
+
         useAuthStore.getState().setTokens(newAccess, refreshToken);
+
         originalRequest.headers['Authorization'] = `Bearer ${newAccess}`;
+
         return axiosClient(originalRequest);
       } catch (err) {
         useAuthStore.getState().logout();
         return Promise.reject(err);
       }
     }
+
     return Promise.reject(error);
   }
 );
